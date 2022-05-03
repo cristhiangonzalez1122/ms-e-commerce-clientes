88facc3a5a81b93d96b80db25bad7bd8c212ae98
cristhian gonzalez <cristhianmg1122@gmail.com>
2022-05-02 22:50:20 -0500

    maked entities and foreignKeys

---
+++ src/models/direccion-cliente.model.ts
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import {Entity, model, property} from '@loopback/repository';
+
+@model({
+  settings: {
+    foreignKeys: {
+      fk_direccionCliente_id_cliente: {
+        name: 'fk_direccionCliente_id_cliente',
+        entity: 'Cliente',
+        entityKey: 'id',
+        foreignKey: 'id_cliente',
+      },
+    },
+  },
+})
+export class DireccionCliente extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nomenclatura: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  numero: string;
+
+  @property({
+    type: 'number',
+  })
+  id_cliente?: number;
+
+  constructor(data?: Partial<DireccionCliente>) {
+    super(data);
+  }
+}
+
+export interface DireccionClienteRelations {
+  // describe navigational properties here
+}
+
+export type DireccionClienteWithRelations = DireccionCliente &
+  DireccionClienteRelations;
